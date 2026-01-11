0de33a98242bc81018550f69cc46a146da439315
Andrey Kotlyar <kotlyar562@gmail.com>
2026-01-11 21:17:33 +0200

    feat(songs): #50 register songs in admin interface

--- apps/songs/admin.py
+++ apps/songs/admin.py
@@ -3,3 +3,16 @@
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
 """Admin settings for songs."""
+
+from django.contrib import admin
+
+from apps.songs.models import Song
+
+
+@admin.register(Song)
+class SongAdmin(admin.ModelAdmin):  # type: ignore[type-arg]
+    """Admin interface for the Song model."""
+
+    filter_horizontal = ("chords", "schemes")
+    list_display = ("title", "metronome")
+    list_editable = ("metronome",)
